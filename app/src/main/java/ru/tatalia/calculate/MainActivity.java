8af8bcfe23444c41f3b24755e919b19245c62719
novaevaX <74986906+novaevaX@users.noreply.github.com>
2023-10-04 17:20:11 +0300

    df
    d

--- app/src/main/java/ru/tatalia/calculate/MainActivity.java
+++ app/src/main/java/ru/tatalia/calculate/MainActivity.java
@@ -3,6 +3,7 @@ package ru.tatalia.calculate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Button but0, but1, but2, but3, but4, but5, but6, but7, but8, but9, butPercent,
             butMinus, butPlus, butMultiply, butDelete, butClear, butDivide, butSolve, butPoint;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView);
         but0 = findViewById(R.id.button_0);
+        but0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str = String.valueOf(textView.getText());
+                str += "0";
+                textView.setText(str);
+            }
+        });
         but1 = findViewById(R.id.button_1);
         but2 = findViewById(R.id.button_2);
         but3 = findViewById(R.id.button_3);
@@ -37,5 +47,9 @@ public class MainActivity extends AppCompatActivity {
         butDivide = findViewById(R.id.button_divide);
         butSolve = findViewById(R.id.button_solve);
         butPoint = findViewById(R.id.button_point);
+    }
+
+    public void onClickButton(View view){
+
     }
 }
